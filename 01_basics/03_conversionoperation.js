1f9105927e7c859b6b8cdc3cbd6018c0ab1f70b0
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-06 18:21:18 +0000

    done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,15 +1,15 @@
 let score="33"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueinnumber=Number(score)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 
 
 let isLoggedIn="Satvik"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //""=>false
@@ -17,5 +17,30 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=28
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//****Operations****
+let value=3
+let negValue= -value
+console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1="hello"
+let str2="mickey"
+
+let str3=str1+str2
+console.log(str3);
+
+//console.log("1"+2);//12
+//console.log(1+"2"); //12
+//console.log(1+2+"2";//32)
+
+console.log((3+4)*4%3);
+console.log(+true);
