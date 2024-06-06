0b91517266ca2f21533f1db9b7f507e3a0818733
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-06 17:07:41 +0000

    done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+console.log("Happy")
+console.log(6+2)
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
