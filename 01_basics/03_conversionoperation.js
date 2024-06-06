48c5cb84529258149c5fff723a59c94cd340d723
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-06 17:34:24 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+
+let isLoggedIn="Satvik"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"minal"=>true
+
+let someNumber=28
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
