f203efe9cf9ef73d889f81086c09e85f92336323
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-07 13:32:27 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log("2">1);
+console.log("02"<1);
+
+
+/****it converts null to a number,treating it as 0 
+that's why null>=0 is true and null>0 is false*****/
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//=== checks datatype 
+console.log("2"===2);
