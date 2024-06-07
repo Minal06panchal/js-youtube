82e5e39972d4b56747b4f4c0b890a1fc4965ebb2
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-07 16:16:09 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name="Minal"
+const repocount=8
+
+//console.log(name+repocount+"value")
+
+//string interpolation
+console.log(`Hello my name is ${name}and my repo count is ${repocount}`);
+
+const getname=new string('Yuvraj')
+console.log(getname[0]);
+console.log(getname.__proto__);
+
+console.log(getname.length);
+console.log(gamename.toUpperCase());
+
+console.log(getname.charAt(2));
+console.log(getname.indexoF('l');
+
+const newString=getname.substring(0,3);
+console.log(newString);
+
+const anotherString=getname.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne="  Mohak  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
