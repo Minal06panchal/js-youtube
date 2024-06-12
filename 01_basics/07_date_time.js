f80c3ae415183c6f573f8d08a835d83f0ee3c913
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-12 15:35:44 +0000

    done

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,30 @@
+let mydate=new Date()
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+
+// let myCreatedDate=new Date(2023,0,12)
+// let myCreatedDate=new Date(2023,0,12,5,15)
+let myCreatedDate=new Date("2024-09-28")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate=new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long";
+})
+
+
+
