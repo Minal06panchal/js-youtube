152d286fcb68104ab537fe40500a368da9da9ec2
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-14 11:55:14 +0000

    done

--- 02_basics/04_obj.js
+++ 02_basics/04_obj.js
@@ -34,10 +34,26 @@ const users=[
  
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+// console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// const{courseInstructor}=course
+const{courseInstructor: instructor}=course
+console.log(instructor);
+
+// {
+//     name:"Petrick",
+//     coursename:"js in hindi",
+//     price:"free"
+// }
