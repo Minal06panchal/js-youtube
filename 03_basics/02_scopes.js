ad6e3593c9352fa9ac827237ef51cd8da4efdacf
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-15 04:41:56 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,5 +4,38 @@ let c=300
   if(true){
     let a=10
     const b=20
-    console.log("INNNER :", a);
+    // console.log("INNNER :", a);
 }
+
+// function one (){
+//   const username="Anjali"
+//   function two(){
+//     const website="youtube"
+//     console.log(username);
+//   }
+  // console.log(website);
+//   two()
+// }
+// one()
+// if(true){
+//   const username="Minal"
+//   if(username==="Minal"){
+//     const website="youtube"
+//     console.log(username+website);
+//   }
+//   console.log(website);
+// }
+// console.log(username);
+
+//++++++++++++++interesting+++++++++++++++++
+console.log(addone(8))
+function addone(num){
+  return num +1
+}
+  
+
+
+// const addTwo=function (num){
+//   return num+2
+// }
+// addTwo(3)
