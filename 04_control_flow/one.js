ae17bbb2e3f35ad8fe63939cd6794dc21b187eb6
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-16 05:47:46 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+//if
+const isUserloggedIn=true
+
+// if(isUserloggedIn){
+
+// }
+3!=2
+// < ,> ,<=, >=,==,===,!=
+
+// const temperature=34
+
+// if(temperature===34){
+//     console.log("less than 50");
+
+// }
+// else{
+//     console.log("temperature is high");
+// }
+// console.log("Execute");
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`);
+// }
+
+const balance=1000
+// if(balance>500) console.log("test"),
+//     console.log("test2");
+
+    if(balance<500){
+        console.log("less than 500");
+    }else if(balance <750)
+        {
+            console.log("less than 750")
+        }
+        else{
+            console.log("less than 1200")
+        }
+
+        const userLoggedIn=true
+        const debitCard=true
+        const loggedInFromGoogle=true
+        const loggedInFromEmail=true
+
+        if(userLoggedIn && debitCard && 2==3 ){
+            console.log("Allow to buy course");
+        }
+
+        if(loggedInFromGoogle||loggedInFromEmail){
+            console.log("user logged in ");
+        }
