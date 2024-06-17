8ccd711464ea64cbb12408aaeee8464310e2da8a
Minal06panchal <136838553+Minal06panchal@users.noreply.github.com>
2024-06-17 06:54:58 +0000

    done

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+const coding=["js","ruby"]
+
+// coding.forEach(function(val)){
+//     console.log(val);
+// }
+
+
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+
+// }
+// coding.forEach(printMe)
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+
+const myCoding=[
+    {
+        languageName:"java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"cpp",
+        languageFileName:"c++"
+    },
+
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+
+
+]
+
+myCoding.forEach((item)=>{
+console.log(item.languageName);
+})
